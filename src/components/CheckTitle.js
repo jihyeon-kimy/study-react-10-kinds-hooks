bd3059a02a882245e257eb293ac70231047c8870
jihyeon-kimy <jihyeon.rem@gmail.com>
2023-02-20 18:19:46 +0900

    3. useTitle : 입력창의 값이 제출되었을 때, Title이 바뀌도록 수정

--- src/components/CheckTitle.js
+++ src/components/CheckTitle.js
@@ -12,12 +12,21 @@ const useTitle = (initialTitle) => {
 
 function CheckTitle() {
   const titleUpdater = useTitle("Loading...");
-  setTimeout(() => {
-    titleUpdater("Home");
-  }, 5000); // 5초후 Title을 Home으로 변경. titleUpdator 확인용
+  // 5초후 Title을 Home으로 변경. titleUpdator 확인용
+  //   setTimeout(() => {
+  //     titleUpdater("Home");
+  //   }, 5000);
   return (
     <>
       <h1>useTitle</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          titleUpdater(event.target.title.value);
+        }}>
+        <input name="title" placeholder="새로운 Title" />
+        <button>수정하기</button>
+      </form>
     </>
   );
 }
